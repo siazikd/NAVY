ea506ec6bdfcdfdbb1cc1c03346d4b868a18c659
Daniel <daniel.siazik@avesoft.cz>
2024-03-26 15:04:50 +0100

    komentare

--- task4.py
+++ task4.py
@@ -3,41 +3,41 @@ import time
 import tkinter as tk
 
 class QLearn:
-    def __init__(self, actions, epsilon=0.1, alpha=0.2, gamma=0.9):
-        self.q = {} 
+    def __init__(self, actions, epsilon=0.05, alpha=0.2, gamma=0.9):
+        self.q = {} # slovnik pro ukladani hodnot Q
         self.epsilon = epsilon # exploration constant
-        self.alpha = alpha  
-        self.gamma = gamma  
-        self.actions = actions  
+        self.alpha = alpha  # learning rate
+        self.gamma = gamma  # discount factor
+        self.actions = actions  # akce, ktere muze agent provest
 
     def getQ(self, state, action):
-        return self.q.get((state, action), 0.0) 
+        return self.q.get((state, action), 0.0)  # pokud neexistuje, tak se vrati 0 
 
     def learnQ(self, state, action, reward, value): 
-        oldv = self.q.get((state, action), None) 
-        if oldv is None: 
+        oldv = self.q.get((state, action), None) # hodnota Q pro stav a akci
+        if oldv is None: # pokud neexistuje, tak se vytvori
             self.q[(state, action)] = reward
-        else: 
-            self.q[(state, action)] = oldv + self.alpha * (value - oldv)
+        else: # jinak se upravi hodnota Q
+            self.q[(state, action)] = oldv + self.alpha * (value - oldv) 
 
     def chooseAction(self, state):
-        if random.random() < self.epsilon: 
+        if random.random() < self.epsilon: # pokud je nahodne cislo mensi nez epsilon, tak se vybere nahodna akce
             action = random.choice(self.actions)
-        else:
+        else: # jinak se vybere akce s nejvetsi hodnotou Q
             q = [self.getQ(state, a) for a in self.actions] 
             maxQ = max(q) 
             count = q.count(maxQ) 
-            if count > 1: 
+            if count > 1:  # pokud je vice akci s nejvetsi hodnotou Q, tak se vybere nahodna z techto akci
                 best = [i for i in range(len(self.actions)) if q[i] == maxQ]
                 i = random.choice(best)
-            else: 
+            else: # jinak se vybere akce s nejvetsi hodnotou Q
                 i = q.index(maxQ)
             action = self.actions[i]
         return action
 
     def learn(self, state1, action1, reward, state2):
-        maxqnew = max([self.getQ(state2, a) for a in self.actions]) 
-        self.learnQ(state1, action1, reward, reward + self.gamma*maxqnew) 
+        maxqnew = max([self.getQ(state2, a) for a in self.actions])  # maximalni hodnota Q pro stav2
+        self.learnQ(state1, action1, reward, reward + self.gamma*maxqnew)  # uprava hodnoty Q pro stav1
         
 
 def print_maze(canvas, maze, mouse_position):
